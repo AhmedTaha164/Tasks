d28aa49648b43c45a6ecfda16129bbf4d58b7bea
AhmedTaha164 <ahmedtaha1642@gmail.com>
2025-07-29 13:34:47 +0300

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-useradd devopstask
-userdel devopstask
+useradd devopstask  #adding new user
+userdel devopstask  #deleting the user
 
-touch newfile
-chmod 733 newfile
-chmod u=rwx,g=rw,o= newfile
+touch newfile  #create new file
+chmod 733 newfile # chnge pirmission for the file 
+chmod u+rwx,g+rw,o-rwx newfile
 
-ls -la
+ls -la # list all the files in the dir
 
-find . -name "newfile"
+find . -name "newfile"  #search for the file in  the current dir
 
-date >> newfile1
+date >> newfile1 #printing the date in the file
 
-cat newfile1
+cat newfile1 #showing the content of the file
 
-alias p='pwd'
+alias p='pwd' #making alias for pwd command
 
-p
+p #show current dir
 
-cp newfile1 /home/ahmed-taha
+cp newfile1 /home/ahmed-taha #copy file for the new dir
 
-mv newfile /home/ahmed-taha
+mv newfile /home/ahmed-taha #move file to for the new dir
 
-touch test1
+touch test1 #create new file
 
-mv test1 file1
+mv test1 file1 #rename the file
 
-mkdir newdir
+mkdir newdir #making new dir
 
-rmdir newdir
+rmdir newdir #removing dir
 
-su taha
+su taha #switch user
